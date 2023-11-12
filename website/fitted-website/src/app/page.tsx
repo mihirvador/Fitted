f641e9902b3d934d81df0d334d905fe85f1c575c
Riya Dev <riyasdev9@gmail.com>
2023-11-12 16:55:45 -0500

    fixed webpack issue.

--- website/fitted-website/src/app/page.tsx
+++ website/fitted-website/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Head from 'next/head'
-import CountryList from '/Users/riyadev/Files/umich/V1/FITTED/Fitted/website/components/CountryList.js'
+import CountryList from '../../components/CountryList'
 
 export default function Home() {
   return (
