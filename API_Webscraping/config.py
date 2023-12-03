b92f6272c5b92e5333e0ef883858f3397fe8d9be
mihirvador <mihirrocks9999@gmail.com>
2023-12-02 23:40:35 -0500

    fastapi

--- API_Webscraping/config.py
+++ API_Webscraping/config.py
@@ -1,16 +1,10 @@
-import pathlib
-import json
 from pydantic_settings import BaseSettings
 from functools import lru_cache
+import os
 
-BASE_DIR = pathlib.Path(__file__).parent
-TOKEN_JSON = str(BASE_DIR / "ignored" / "mihirrocks9999@gmail.com-token.json")
 
-with open(TOKEN_JSON) as f:
-    secrets = json.load(f)
-
-CLIENT_ID = secrets["clientId"]
-CLIENT_SECRET = secrets["secret"]
+CLIENT_ID = os.environ.get('clientId')
+CLIENT_SECRET = os.environ.get('secret')
 
 class Settings(BaseSettings):
     names: str = "FittedAPI"
